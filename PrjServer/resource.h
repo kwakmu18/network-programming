9e2eb51771f91b703b8e96e8722da5d4244c8d02
kwakmu18 <mckkk119@naver.com>
2023-11-30 22:59:46 +0900

    initial commit
    Add client, server program

---
+++ PrjServer/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// PrjServer.rc에서 사용되고 있습니다.
+//
+#define IDD_DIALOG1                     101
+#define IDC_USERLIST                    1001
+#define IDC_TextBox1                    1004
+#define IDC_TextBox2                    1005
+#define IDC_TextBox3                    1006
+#define IDC_TextBox4                    1007
+#define IDC_USERID                      1008
+#define IDC_USERIP                      1009
+#define IDC_USERPORT                    1010
+#define IDC_USERPROTO                   1011
+#define IDC_BUTTON1                     1012
+#define IDC_USERLOG                     1013
+#define IDC_EDIT2                       1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
